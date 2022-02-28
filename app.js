7cf46529b7ee1c139a023ff93539b9e4c75d0544
Roshni <roshnigupta8@gmail.com>
2022-02-28 17:17:04 +0000

    added student events in app.js

--- app.js
+++ app.js
@@ -1,88 +1,53 @@
-require("./db/connect");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
-
-//trying
-const server = require('http').createServer(app);
+require("dotenv").config();
+const http = require("http");
+app.server = http.createServer(app);
 const options = {
   cors: true,
-  origins: ['http://localhost:3000/presentations/'],
+  origins: ["http://localhost:3000/presentations/"],
 };
-const io = require('socket.io')(server, options);
-
-
-//studedent
-const options1 = {
-  cors: true,
-  origins: ['http://localhost:3000/Form'],
-};
-
-const io1 = require('socket.io')(server, options1);
-
-io1.on('connection', (socket) => {
-  console.log(`Student connected ${socket.id}`);
-
-  // socket.on('current_session', (data) => {
-  //   console.log(data)
-  //   //socket.to(student).emit('receive_message', data) 
-  //   //io.emit('receive_message', data)  // this need to emit to student portal
-  // })
-
-  // when someone leaves the chat room
-  socket.on("disconnect", () => {
-    console.log("student disconnected", socket.id)
-  })
-});
-
-io.on('connection', (socket) => {
-  console.log(`User connected ${socket.id}`);
-
-  socket.on('current_session', (data) => {
-    console.log(data)
-    //socket.to(student).emit('receive_message', data) 
-    //io.emit('receive_message', data)  // this need to emit to student portal
-  })
-
-  // when someone leaves the chat room
-  socket.on("disconnect", () => {
-    console.log("user disconnected", socket.id)
-  })
-});
-server.listen(3002, () => {
-  console.log('listening on *: http://localhost:3002');
-});
-//////
-
+io = require("socket.io")(app.server, options);
 const home = require("./routes/home");
 const apiInfo = require("./routes/apiInfo");
-const results = require("./routes/results");
+const presentations = require("./routes/presentations");
 
-const connectDB = require("./db/connect");
-
-require("dotenv").config();
-
-const { handleInvalidUrlErrors, handleCustomErrors, handleServerErrors } = require("./error-handler/error");
+const {
+  handleInvalidUrlErrors,
+  handleCustomErrors,
+  handleServerErrors,
+} = require("./error-handler/error");
 
 app.use(express.json());
 
 app.use("/", home);
 app.use("/api", apiInfo);
-app.use("/api/results", results);
+app.use("/api/presentations", presentations);
 
 app.all("*", handleInvalidUrlErrors);
 app.use(handleCustomErrors);
 app.use(handleServerErrors);
+io.on("connection", (socket) => {
+  console.log(`User connected ${socket.id}`);
 
-let PORT = process.env.PORT || 3001;
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(PORT);
-  } catch (error) {
-    console.log(error);
-  }
-};
-start();
+  socket.on('student_login', (data) => {
+    console.log(data)
+    // socket.to(data).emit('receive_message', data)
+    io.emit('current_slide', 'slide_id_123')
+  })
+
+  socket.on('test', (data) => {
+    console.log(data, '<<from student fe')
+  })
+
+  socket.on("current_session", (data) => {
+    console.log(data);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
+
 module.exports = app;
